7d7df35a55f35fe92eec130bfbbd554794ec627e
Daniel Ramos <dramosg@ucentral.edu.co>
2024-09-08 19:40:20 -0500

    Se añade bloque statico

--- src/main/java/org/example/chainresponsability/model/ModelTest.java
+++ src/main/java/org/example/chainresponsability/model/ModelTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class ModelTest extends SuperClass{
     private static final List<ValidationRule<ModelRequest, ModelTest>> rules =  new ArrayList<>(10);
 
-    {
+    static {
         rules.add(new ValidationRule<>(model -> !model.getPrices().isEmpty(), model -> model.setName("Name")));
         rules.add(new ValidationRule<>(model -> model.getPrices().isEmpty(), model -> model.setActive(true)));
     }
